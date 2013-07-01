21f6d59c22662a9bb9b4a18d35c2126c2f1adcca
Luke Orland <orluke@gmail.com>
2013-07-01 14:11:40 -0400

    add --nltk option to scripts/split_sentences.py

--- scripts/split_sentences.py
+++ scripts/split_sentences.py
@@ -4,35 +4,49 @@
 # one sentence per line. It skips suspected SGML markup (any
 # line beginning with '<'). All other lines not separated 
 # by an extra line break are concatenated and then split 
-# with Splitta 1.03.
+# with Splitta 1.03 by default, or use the command line argurment --nltk to use
+# the NLTK English sentence splitter.
 #
 # cat sample.xml | python split_sentences.py
 #
 # Courtney Napoles, cdnapoles@gmail.com
 # 2012-06-29
 
-import sys, sbd, os
+import sys, os
 
-model_path = os.path.dirname(sbd.__file__)+'/model_svm/'
-model = sbd.load_sbd_model(model_path,True)
+if len(sys.argv) > 1 and sys.argv[1] == '--nltk':
+    import nltk_english_sentence_splitter
+    splitter = 'NLTK'
+else:
+    import sbd
+    splitter = 'Splitta'
+    model_path = os.path.dirname(sbd.__file__)+'/model_svm/'
+    model = sbd.load_sbd_model(model_path,True)
+
 
 def split(lines) :
     if len(lines) > 0 :
-        test = sbd.get_text_data(' '.join(lines),tokenize=True)
-        test.featurize(model)
-        model.classify(test)
-        split_sentences = test.segment(use_preds=True,list_only=True)
-        # Splitta will drop the last sentence, apparently at random.
-        # We will look for dropped sentences by offset and append them
-        # to the split sentence ist. Recursively re-splitting missed 
-        # sentences does not work because Splitta still will not 
-        # recognize the dropped sentences
-        new_length = len(' '.join(split_sentences))
-        old_length = len(' '.join(lines))
-        if new_length != old_length :
-            split_sentences.append(' '.join(lines)[new_length:])
-            sys.stderr.write('SBD ERROR\t'+' '.join(lines)+'\n')
-        for s in split_sentences :
+        text = ' '.join(lines)
+        if splitter is 'Splitta':
+            test = sbd.get_text_data(text, tokenize=True)
+            test.featurize(model)
+            model.classify(test)
+            split_sentences = test.segment(use_preds=True,list_only=True)
+            # Splitta will drop the last sentence, apparently at random.
+            # We will look for dropped sentences by offset and append them
+            # to the split sentence ist. Recursively re-splitting missed 
+            # sentences does not work because Splitta still will not 
+            # recognize the dropped sentences
+            new_length = len(' '.join(split_sentences))
+            old_length = len(' '.join(lines))
+            if new_length != old_length :
+                split_sentences.append(' '.join(lines)[new_length:])
+                sys.stderr.write('SBD ERROR\t'+' '.join(lines)+'\n')
+        else:
+            # Use NLTK to split sentences.
+            split_sentences = nltk_english_sentence_splitter.split_sentences(text)
+
+        for s in split_sentences:
             print s
 
 lines = []
